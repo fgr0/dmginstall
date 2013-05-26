585bb52dbb7b2e1416a619d4a1d830040bef63ec
Franz Greiling <git@lc3dyr.de>
2013-05-26 22:52:16 +0200

    Initial Commit

---
+++ workflow.py
@@ -0,0 +1,129 @@
+# DMG Install Workflow for Alfred v2
+#
+# -----------------------------------------------------------------------------
+# "THE NERD-WARE LICENSE" (Revision 1): <dev.git@lc3dyr.de> wrote this file.
+# As long as you retain this notice you can do whatever you want with this
+# stuff. If we meet some day, and you think this stuff is worth it, you can buy
+# me a beer, mate or some food in return [Franz Greiling]
+# -----------------------------------------------------------------------------
+#   
+# by laerador (Franz Greiling)
+
+import alp
+
+import sys
+import os.path
+import time
+
+import errno
+import subprocess
+
+import shutil
+
+
+'''
+Basic Functions for Finding, Mounting and Installing
+'''
+def find_dmg(directory='~/Downloads/'):
+    """ Searches 'directory' for all files ending with *.dmg """
+    directory = os.path.expanduser(directory)
+
+    # Find all DMGs
+    dmgs = []
+    for f in os.listdir(directory):
+        if f.endswith('.dmg'):
+            dmgs.append(os.path.join(directory, f))
+
+    # Sort by Creation time; Newest come first
+    dmgs = sorted(dmgs, key=lambda f: os.path.getctime(f), reverse=True)
+
+    return dmgs
+
+
+def find_install_app(directory, installto='/Applications/'):
+    """ Finds an App in 'path' and installs it """
+    
+    # Find all DMGs
+    apps = []
+    for f in os.listdir(directory):
+        if f.endswith('.app'):
+            apps.append(os.path.join(directory, f))
+
+    # Copy all apps to Install-Directory
+    for app in apps:
+        return_code = subprocess.call(['cp', '-a', app, installto])
+        if return_code != 0:
+            print(errno.errorcode(return_code))
+            print(return_code)
+            sys.exit(1)
+
+    return len(apps)
+
+
+def mount_dmg(dmg):
+    """ Mounts given DMG at /Volumes/NAME """
+    
+    # Generate Mountpoint
+    mount_point = os.path.join('/Volumes/', os.path.splitext(os.path.basename(dmg))[0])
+
+    # Mount dmg
+    dnull = open('/dev/null','w')
+    return_code = subprocess.call(['hdiutil', 'attach', '-mountpoint', mount_point, dmg], stdout=dnull)
+    
+    if return_code is not 0:
+        print(errno.errorcode(return_code))
+        sys.exit(1)
+
+    return mount_point
+
+
+def unmount_volume(volume, dmg=None):
+    if dmg:
+        volume = os.path.join('/Volumes/', os.path.splitext(os.path.basename(dmg))[0])
+
+    # Unmount Volume
+    dnull = open('/dev/null','w')
+    return_code = subprocess.call(['hdiutil', 'detach', volume], stdout=dnull)
+
+    if return_code is not 0:
+        print(errno.errorcode(return_code))
+        sys.exit(1)
+
+
+'''
+Functions for the use with Alfred
+'''
+def file_action(dmg):
+    vol = mount_dmg(dmg)
+    nr = find_install_app(vol)
+    unmount_volume(vol)
+
+    if nr is 0:
+        print('No Apps where installed')
+    elif nr is 1:
+        print('1 App was installed')
+    elif nr > 0:
+        print('%d Apps where installed'%nr)
+
+def keyword_action():
+    dmgs = find_dmg()
+    if dmgs:
+        file_action(dmgs[0])
+
+def script_action():
+    """Creates Alfred-Feedback for the most-recent DMG"""
+    dmgs = find_dmg()
+
+    if dmgs:
+        dmg = dmgs[0]
+        alp.feedback(alp.Item(**{
+            'title': 'Install %s'%(os.path.splitext(os.path.basename(dmg))[0],),
+            'subtitle': 'Installs all Apps from %s'%dmg,
+            'arg': dmg,
+            'fileIcon': dmg,
+            'fileType': 'True'}))
+    else:
+        alp.feedback(alp.Item(**{
+            'title': 'Install dmg',
+            'subtitle': 'No dmg found',
+            'valid': no}))
