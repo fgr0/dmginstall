3d55f5646c6611e73b7e68d47e51f18bb813c035
Franz Greiling <git@lc3dyr.de>
2013-05-28 19:33:04 +0200

    Add ability to install pkgs and single Application

--- workflow.py
+++ workflow.py
@@ -51,28 +51,48 @@ def find_zip(directory='~/Downloads/'):
             zf = zipfile.ZipFile(os.path.join(directory, f), 'r')
             if [x.split('.app/', 1)[0]+'.app/' for x in zf.namelist() if x.count('.app/') == 1]:
                 zips.append(os.path.join(directory, f))
+            elif [x for x in zf.namelist() if x.endswith('.pkg')]:
+                zips.append(os.path.join(directory, f))
 
     return zips
 
 
-def find_install_app(directory, installto='/Applications/'):
+def find_install_app(directory='~/Downloads/', one=None, install=True, installto='/Applications/'):
     """ Finds an App in 'path' and installs it """
+    directory = os.path.expanduser(directory)
     
-    # Find all DMGs
-    apps = []
-    for f in os.listdir(directory):
-        if f.endswith('.app'):
-            apps.append(os.path.join(directory, f))
+    if not one:
+        # Find all Apps in location
+        apps = []
+        for f in os.listdir(directory):
+            if f.endswith('.app') or f.endswith('.pkg'):
+                apps.append(os.path.join(directory, f))
+    elif os.path.exists(one):
+        apps = [one]
 
-    # Copy all apps to Install-Directory
-    for app in apps:
-        return_code = subprocess.call(['cp', '-pR', app, installto])
-        if return_code != 0:
-            print(errno.errorcode[return_code])
-            print(return_code)
-            sys.exit(1)
+    if install:
+        # Copy all apps to Install-Directory
+        for app in apps:
+            if app.endswith('.pkg'):
+                install_pkg(app)
+            else:
+                return_code = subprocess.call(['cp', '-pR', app, installto])
+                if return_code != 0:
+                    print(errno.errorcode[return_code])
+                    print(return_code)
+                    sys.exit(1)
 
-    return len(apps)
+        return len(apps)
+    else:
+        return apps
+
+def install_pkg(pkg):
+    return_code = subprocess.call(['open', '-W', pkg])
+    if return_code != 0:
+        print(errno.errorcode[return_code])
+        print(return_code)
+        sys.exit(1)
+    return 1
 
 
 def mount_dmg(dmg,unmount=False):
@@ -99,14 +119,16 @@ def extract_from_zip(zipf):
     
     zf = zipfile.ZipFile(zipf,'r')
     d = mkdtemp()
-    for app in list(set([x.split('.app/',1)[0]+'.app/' for x in zf.namelist() if x.count('.app/') == 1])):
+    apps = [x.split('.app/',1)[0]+'.app/' for x in zf.namelist() if x.count('.app/') == 1]
+    apps+= [x for x in zf.namelist() if x.endswith('.pkg')]
+    for app in list(set(apps)):
         return_code = subprocess.call(['unzip', '%s'%zipf, '%s'%app, '-d', '%s'%d])
         if return_code != 0:
             print(errno.errorcode[return_code])
             print(return_code)
             sys.exit(1)
 
-    return find_install_app(d)
+    return d
 
 
 '''
@@ -114,12 +136,21 @@ Functions for the use with Alfred
 '''
 def file_action(f,delete=False):
     if f.endswith('.dmg'):
+        # Install from dmgs
         vol = mount_dmg(f)
         nr = find_install_app(vol)
         mount_dmg(f,unmount=True)
     elif f.endswith('.zip'):
-        nr = extract_from_zip(f)
+        # Install from zip
+        d = extract_from_zip(f)
+        nr = find_install_app(d)
+    elif f.endswith('.app') or f.endswith('.pkg'):
+        # Move app
+        nr = find_install_app(one=f)
+    else:
+        nr = 0
 
+    # Feedback for Notification
     if nr is 0:
         print('No Apps where installed')
         return
@@ -128,6 +159,7 @@ def file_action(f,delete=False):
     elif nr > 0:
         print('%d Apps where installed'%nr)
 
+    # Remove if wanted
     if delete:
         try:
             send2trash(f)
@@ -135,9 +167,10 @@ def file_action(f,delete=False):
             pass
 
 def script_action():
-    """Creates Alfred-Feedback for the most-recent DMG"""
+    """Creates Alfred-Feedback for the most-recent DMG/ZIP/APP"""
     matches = find_dmg()
     matches+= find_zip()
+    matches+= find_install_app(install=False)
 
     # Sort by Creation time; Newest come first
     matches = sorted(matches, key=lambda f: os.path.getctime(f), reverse=True)
@@ -145,12 +178,20 @@ def script_action():
     fb = []
     if matches:
         for match in matches:
-            fb.append(alp.Item(**{
-                'title': 'Install %s'%(os.path.splitext(os.path.basename(match))[0],),
-                'subtitle': 'Install all Apps from %s'%match,
-                'arg': match,
-                'fileIcon': match,
-                'fileType': 'True'}))
+            if match.endswith('.app'):
+                fb.append(alp.Item(**{
+                    'title': 'Install %s'%(os.path.splitext(os.path.basename(match))[0],),
+                    'subtitle': 'Install this App',
+                    'arg': match,
+                    'fileIcon': match,
+                    'fileType': 'True'}))
+            else:
+                fb.append(alp.Item(**{
+                    'title': 'Install %s'%(os.path.splitext(os.path.basename(match))[0],),
+                    'subtitle': 'Install all Apps from %s'%match,
+                    'arg': match,
+                    'fileIcon': match,
+                    'fileType': 'True'}))
     else:
         fb.append(alp.Item(**{
             'title': 'Install dmg',
